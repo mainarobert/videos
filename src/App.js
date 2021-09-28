555efddb0b66ca5b5fede8d42800b2dcfa3bfccc
RobertMaina <robertmaina254@gmail.com>
2021-09-28 21:59:13 +0300

    add components folder with Search.js and add semantic ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
+import React, { Component } from 'react'
+import Search from './components/Search'
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Search />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
---
+++ src/components/Search.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+class Search extends Component {
+    state = {
+        display: ''
+    }
+
+    render() {
+        return(
+            <div className="search-bar ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label>Search Video</label>
+                        <input 
+                            type="text"
+                            value={ this.state.display }
+                            onChange= { e => this.setState({ display: e.target.value })  }  // update state:display property with setState() & assign e to state property
+                        />
+                    </div>
+                    <button onClick={ e => e.preventDefault() }>Search</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Search
