1f4b3b7b997ab5ff03ab7534d33c57885252d7f2
RobertMaina <robertmaina254@gmail.com>
2021-09-30 20:11:08 +0300

    make request to youtube api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React, { Component } from 'react'
-import Search from './components/Search'
+import React, { Component } from 'react';
+import Search from './components/Search';
+import Youtube from './api/Youtube';
 
 class App extends Component {
+  //callled when user submits search form
+  onTermSubmit = (term) => {
+    // Youtube is an instance of axios since its pre-configured in Youtube.js
+    // '/search' is the path we want to make get request to
+    Youtube.get('/search', {
+      params: {
+        //q is a property based off youtube api doc
+        q: term
+      }
+    })
+  }
+
   render() {
     return (
       <div>
-        <Search />
+        <Search onFormSubmit= {this.onTermSubmit} />    {/* pass onTermSubmit callback as props */}
       </div>
     )
   }
--- src/components/Search.js
+++ src/components/Search.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Search extends Component {
     state = {
-        display: ''
+        term: ''
     }
 
     render() {
@@ -13,11 +13,16 @@ class Search extends Component {
                         <label>Search Video</label>
                         <input 
                             type="text"
-                            value={ this.state.display }
-                            onChange= { e => this.setState({ display: e.target.value })  }  // update state: display property with setState() & assign e to state property to target empty string
+                            value={ this.state.term }
+                            onChange= { e => this.setState({ term: e.target.value })  }  // update state: display property with setState() & assign e to state property to target empty string
                         />
                     </div>
-                    <button onClick={ e => e.preventDefault() }>Search</button>
+                    <button onClick={ (e) => {
+                        e.preventDefault();
+                        this.props.onFormSubmit(this.state.term)
+                    }}>
+                        Search
+                    </button>
                 </form>
             </div>
         )
