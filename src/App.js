a4a6b1a0f87fbc1dd67ca5400466e8f926357167
RobertMaina <robertmaina254@gmail.com>
2021-10-01 20:03:01 +0300

    update state with fetched data in app component

--- src/App.js
+++ src/App.js
@@ -3,22 +3,30 @@ import Search from './components/Search';
 import Youtube from './api/Youtube';
 
 class App extends Component {
+  state = {
+    videos: []
+  }
+
   //callled when user submits search form
-  onTermSubmit = (term) => {
+  onTermSubmit = async term => {
     // Youtube is an instance of axios since its pre-configured in Youtube.js
     // '/search' is the path we want to make get request to
-    Youtube.get('/search', {
+    const response = await Youtube.get('/search', {
       params: {
         //q is a property based off youtube api doc
         q: term
       }
     })
+    
+    this.setState({videos: response.data.items})
+    console.log(response.data.items)
   }
 
   render() {
     return (
       <div>
         <Search onFormSubmit= {this.onTermSubmit} />    {/* pass onTermSubmit callback as props */}
+        found {this.state.videos.length} videos
       </div>
     )
   }
